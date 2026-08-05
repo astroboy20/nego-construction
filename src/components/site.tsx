d3ddf2a709fd3c7d6e12d98bf652abaad8723d6f
astroboy20 <tolulopeakinkunmi7@gmail.com>
2026-08-05 18:06:31 +0100

    style: increase logo height for improved visibility

--- src/components/site.tsx
+++ src/components/site.tsx
@@ -35,7 +35,7 @@ export function Logo() {
         alt="NEGO Construction"
         width={120}
         height={40}
-        className="h-10 w-auto object-contain"
+        className="h-14 w-auto object-contain"
         priority
       />
     </Link>
